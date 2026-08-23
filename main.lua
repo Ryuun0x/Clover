d5be74be927ca3af7733722ffdcfd27bc9847bd2
Ryuu <pajarillagajonard@gmail.com>
2026-08-23 22:34:27 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,16 +3,16 @@
 local BASE_URL = "https://raw.githubusercontent.com/Ryuun0x/VoidHub/refs/heads/main/"
 
 local ROUTES = {
-    [126884695634066] = { name = "Garden World", file = "GardenWorld.lua" },
-    [124977557560410] = { name = "Garden World", file = "GardenWorld.lua" },
-    [129954712878723] = { name = "Trading World", file = "TradingWorld.lua" },
-    [108890465381067] = { name = "Trading World", file = "TradingWorld.lua" },
-    [97598239454123] = { name = "Garden World 2", file = "void.lua" },
-    [77085202503540] = { name = "Garden World 2", file = "void.lua" },
-    [107778070777162] = { name = "Steal An Egg", file = "CAE.lua" },
+    ["126884695634066"] = { name = "Garden World", file = "GardenWorld.lua" },
+    ["124977557560410"] = { name = "Garden World", file = "GardenWorld.lua" },
+    ["129954712878723"] = { name = "Trading World", file = "TradingWorld.lua" },
+    ["108890465381067"] = { name = "Trading World", file = "TradingWorld.lua" },
+    ["97598239454123"] = { name = "Garden World 2", file = "void.lua" },
+    ["77085202503540"] = { name = "Garden World 2", file = "void.lua" },
+    ["107778070777162"] = { name = "Steal An Egg", file = "CAE.lua" },
 }
 
-local route = ROUTES[game.PlaceId]
+local route = ROUTES[tostring(game.PlaceId)]
 if not route then
     warn(string.format("[VoidHub] Unsupported game. PlaceId: %s", tostring(game.PlaceId)))
     return
